0a307e8c61d1b7fa267e5048e44eec7258637c09
cheng cheng <736113252@qq.com>
2020-07-23 18:30:38 +0800

    0723firstcommit

---
+++ ORMModel/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ORMModel
+{
+    public class Company:BaseModel
+    {
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int CreatorId { get; set; }
+        public int LastModifierId { get; set; }
+        public DateTime LastModifyTime { get; set; }
+    }
+}
